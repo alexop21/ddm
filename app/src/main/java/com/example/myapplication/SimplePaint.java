98cbbea0ef6f14363395221013ee791b512b1cdc
alexop21 <alexandrepontes544@gmail.com>
2026-05-16 17:09:31 -0300

    Paint add

--- app/src/main/java/com/example/myapplication/SimplePaint.java
+++ app/src/main/java/com/example/myapplication/SimplePaint.java
@@ -1,13 +1,65 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class SimplePaint extends View {
+    Path mPath;
+    Paint mPaint;
+    public SimplePaint(Context context) {
+        super(context);
+    }
+
+    public SimplePaint(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
     public SimplePaint(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+
+    }
+    public void init(){
+        mPath= new Path();
+        mPaint= new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(10);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(mPath,mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.lineTo(event.getX(),event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(),event.getY());
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+        }
+
+        invalidate();
+        return true;
     }
 }
