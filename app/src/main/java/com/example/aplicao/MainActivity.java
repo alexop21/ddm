20619df4c1784e4cd8eefffe765f2e10d07deb7f
alexop21 <alexandrepontes544@gmail.com>
2026-05-28 21:21:28 -0300

    aula começo

---
+++ app/src/main/java/com/example/aplicao/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.aplicao;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    String nomes[] = new String[] {"Joao","Anna","Hudson","Xibilú"};
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        lv= findViewById(R.id.listView);
+        ArrayAdapter<String> adapter =new ArrayAdapter<>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+        );
+        lv.setAdapter(adapter);
+
+    }
+}
