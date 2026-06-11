4a44696cfade1155d99838694bfe32a6997604b4
unknown <alexandrepontes544@gmail.com>
2026-06-10 21:38:23 -0300

    ajustes da aula

--- app/src/main/java/com/example/aplicao/MainActivity.java
+++ app/src/main/java/com/example/aplicao/MainActivity.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,15 +24,29 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase sqLiteDatabase;
+    ListView listView;
+    EditText editText;
+    Button button;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        ListView listView;
-
         listView=findViewById(R.id.listView);
+        button=findViewById(R.id.button);
+        editText=findViewById(R.id.editTextTitle);
+
+        button.setOnClickListener(v -> {
+            String string = editText.getText().toString();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("id",1);
+            contentValues.put("titulo","Nota 1");
+            sqLiteDatabase.insert("notas",null,contentValues);
+            adapter.notifyDataSetChanged();
+            ListarDB();
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -38,27 +54,21 @@ public class MainActivity extends AppCompatActivity {
         });
         sqLiteDatabase=openOrCreateDatabase("banco.db",MODE_PRIVATE,null);
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas(id INTEGER PRIMARY KEY AUTOINCREMENT,titulo TEXT, conteudo TEXT)");
-        //sqLiteDatabase.execSQL("INSERT INTO notas (id,titulo,conteudo) VALUES (1,'Nota 1', 'Conteudo da nota 1')");
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("id",1);
-        contentValues.put("titulo","Nota 1");
-        contentValues.put("conteudo","Conteudo da nota 1");
-        sqLiteDatabase.insert("notas",null,contentValues);
-
-        //RECUPERAR DADOS DO BANCO
+        ListarDB();
+    }
+    public void ListarDB(){
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM notas",null);
         cursor.moveToFirst();
-
-        ArrayList<String> lista = new ArrayList<>();
+        lista = new ArrayList<>();
         while(!cursor.isAfterLast()){
             int id = cursor.getInt(cursor.getColumnIndex("id"));
-            String titulo= cursor.getString(cursor.getColumnIndex(("titulo"));
+            String titulo= cursor.getString(cursor.getColumnIndex("titulo"));
             String conteudo= cursor.getString(cursor.getColumnIndex("conteudo"));
             lista.add("id: "+Integer.toString(id)+" titulo: "+titulo);
             cursor.moveToNext();
             Log.d("SELECT notas","id: "+id+" titulo: "+titulo+" conteudo : "+conteudo);
         }
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,lista);
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,lista);
         listView.setAdapter(adapter);
     }
 }
